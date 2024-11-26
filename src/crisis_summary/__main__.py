cea583dd168eddc41164fabe4809c848392c011f
JaeHoBahng <jaeho127@gmail.com>
2024-11-26 08:50:08 -0500

    #2 change util location clean repository

--- src/crisis_summary/__main__.py
+++ src/crisis_summary/__main__.py
@@ -1,5 +1,5 @@
 from crisis_summary.summary import crisis
-from utils.util import get_eventsMeta
+from crisis_summary.utils.util import get_eventsMeta
 
 from dotenv import load_dotenv
 import os
@@ -14,7 +14,7 @@ import os
 
 # I think it's a good idea to have a y/n input for whether the user wants to have a summary because the summary needs an api key.
 
-# Save to csv y/n      This isn't a pre-made function maybe we can just put it as part of the main.py file
+# Save to csv y/n      This isn't a pre-made function maybe we can just put it as part of the main.py file and make it save to the output repo
 
 load_dotenv('../../.env')
 os.environ['IR_DATASETS_HOME'] = '../../'
