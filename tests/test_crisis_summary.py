96baa79aa93997705b47985be0586856a5249adc
JaeHoBahng <jaeho127@gmail.com>
2024-11-26 09:03:43 -0500

    small change

--- tests/test_crisis_summary.py
+++ tests/test_crisis_summary.py
@@ -1,8 +1,5 @@
 from crisis_summary.summary import crisis
-from utils.util import get_eventsMeta
-
-from crisis_summary.summary import crisis
-from utils.util import get_eventsMeta
+from crisis_summary.utils.util import get_eventsMeta
 
 import os
 
