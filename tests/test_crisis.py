03778560b01c0195a39c2740e7d3b66df5c9ba1a
JaeHoBahng <jaeho127@gmail.com>
2024-12-01 16:31:49 -0500

    simplify test

--- tests/test_crisis.py
+++ tests/test_crisis.py
@@ -6,42 +6,42 @@ import ir_datasets
 import json
 import os
 
-os.environ['IR_DATASETS_HOME'] = './'
+# os.environ['IR_DATASETS_HOME'] = './'
 
-# Ensure the dataset directory exists
-if not os.path.exists(os.environ['IR_DATASETS_HOME']):
-    os.makedirs(os.environ['IR_DATASETS_HOME'])
+# # Ensure the dataset directory exists
+# if not os.path.exists(os.environ['IR_DATASETS_HOME']):
+#     os.makedirs(os.environ['IR_DATASETS_HOME'])
 
 def test_columns():
 
-    os.environ['IR_DATASETS_HOME'] = './'
+    # os.environ['IR_DATASETS_HOME'] = './'
 
-    os.makedirs('auth', exist_ok=True)
+    # os.makedirs('auth', exist_ok=True)
 
-    credentials = {
-        "institution": "Georgetown University", # University, Company or Public Agency Name
-        "contactname": "JaeHo Bahng", # Your Name
-        "email": "jaheo127@gmail.com", # A contact email address
-        "institutiontype": "Academic" # Either 'Research', 'Industry', or 'Public Sector'
-    }
+    # credentials = {
+    #     "institution": "Georgetown University", # University, Company or Public Agency Name
+    #     "contactname": "JaeHo Bahng", # Your Name
+    #     "email": "jaheo127@gmail.com", # A contact email address
+    #     "institutiontype": "Academic" # Either 'Research', 'Industry', or 'Public Sector'
+    # }
 
-    with open('./auth/crisisfacts.json', 'w') as f:
-        json.dump(credentials, f)
+    # with open('./auth/crisisfacts.json', 'w') as f:
+    #     json.dump(credentials, f)
 
     os.environ['IR_DATASETS_HOME'] = './'
 
     eventsMeta = get_eventsMeta(eventNoList='001', days=1)
 
-    for eventId,dailyInfo in eventsMeta.items():
+    # for eventId,dailyInfo in eventsMeta.items():
 
-        for thisDay in dailyInfo:
+    #     for thisDay in dailyInfo:
             
-            requestID = thisDay["requestID"]
-            ir_dataset_id = "crisisfacts/%s/%s" % (eventId, thisDay["dateString"])        
+    #         requestID = thisDay["requestID"]
+    #         ir_dataset_id = "crisisfacts/%s/%s" % (eventId, thisDay["dateString"])        
             
-            dataset = ir_datasets.load(ir_dataset_id)
-            itemsAsDataFrame = dataset.docs_iter()
-            queries = dataset.queries_iter()
+    #         dataset = ir_datasets.load(ir_dataset_id)
+    #         itemsAsDataFrame = dataset.docs_iter()
+    #         queries = dataset.queries_iter()
 
 
 
