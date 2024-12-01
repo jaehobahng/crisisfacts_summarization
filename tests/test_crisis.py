ea32c574333d9b1d1fdae7844acb840284d9af55
JaeHoBahng <jaeho127@gmail.com>
2024-12-01 10:19:17 -0500

    ir environment change

--- tests/test_crisis.py
+++ tests/test_crisis.py
@@ -5,24 +5,11 @@ from rerankers import Reranker
 import json
 import os
 
-# Create the 'auth' directory
-os.makedirs('auth', exist_ok=True)
-
-credentials = {
-    "institution": "Georgetown University", # University, Company or Public Agency Name
-    "contactname": "JaeHo Bahng", # Your Name
-    "email": "jaheo127@gmail.com", # A contact email address
-    "institutiontype": "Academic" # Either 'Research', 'Industry', or 'Public Sector'
-}
-
-# Write this to a file so it can be read when needed
-
-with open('./auth/crisisfacts.json', 'w') as f:
-    json.dump(credentials, f)
+os.environ['IR_DATASETS_HOME'] = './'
 
 
 def test_columns():
-    
+
     os.environ['IR_DATASETS_HOME'] = './'
 
     eventsMeta = get_eventsMeta(eventNoList='001', days=1)
